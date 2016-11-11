b1e80c46727ddd7b0f4ee90b21c77d5572b68f06
autre <b.alexandrou@protonmail.ch>
2016-11-11 15:33:47 +0200

    removing ansible.sh and appspec

--- ansible.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -H "Authorization: token e373019cc03bd85cf4ba6e227be5baa9e52d3e56" \
-  -H "Accept: application/vnd.github.v3.raw" \
-  -L https://api.github.com/repos/Avocarrot/devops/contents/startup-scripts/ansible.sh | sh -s $(dirname $0)
