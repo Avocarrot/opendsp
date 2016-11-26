3a85aa8e46b7babbbd9e2e9e55f356677cfd29d5
autre <b.alexandrou@protonmail.ch>
2016-11-26 15:43:24 +0200

    adding support for probabilistic empty bid respones

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var DSP = require('./lib/dsp');
 var server = new DSP({
   lag: (nconf.get('server:lag') || 0),
   details: (nconf.get('dsp') || { name: 'jack-hammerton' }),
-  timeout: (nconf.get('server:timeout') || 0)
+  timeout: (nconf.get('server:timeout') || 0),
+  empty_bid: (nconf.get('server:empty_bid') || 0)
 });
 
 server.listen((nconf.get('port') || nconf.get('server:port') || 3000));
